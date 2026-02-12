7db01974500c3a1c538be39a7799400113448ca9
Magali JOUAN <jouan.magali@gmail.com>
2026-02-12 15:05:22 +0100

    modification de l'url

--- vite.config.js
+++ vite.config.js
@@ -15,13 +15,13 @@ export default defineConfig({
         }),
     ],
     server: {
-        host: 'meeplix2.local', // virtualhost défini dans /etc/hosts ou Valet
+        host: 'meeplix_steph.local', // virtualhost défini dans /etc/hosts ou Valet
         port: 5173,
-        origin: 'http://meeplix2.local:5173', // <- FORCE l'origin injectée dans le CSS
+        origin: 'http://meeplix_steph.local:5173', // <- FORCE l'origin injectée dans le CSS
         cors: true,
         strictPort: true,
         hmr: {
-            host: 'meeplix2.local',
+            host: 'meeplix_steph.local',
             protocol: 'ws',
         },
     }
